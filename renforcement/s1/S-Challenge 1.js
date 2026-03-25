6fb111a8281bfbd0ea3547fd89ada496a38ebcce
makhfi03 <ghita.makhfi.21@gmail.com>
2026-03-25 11:42:01 +0100

    challenge1 jour 1

---
+++ renforcement/s1/S-Challenge 1.js
@@ -0,0 +1,9 @@
+// Tu gères les inscriptions à un événement. Crée un tableau contenant 6 noms de participants. 
+// Affiche la liste complète, le premier inscrit, le dernier inscrit et le nombre 
+// total de participants. Ensuite, ajoute 2 nouveaux participants à la fin de la liste, 
+// retire le dernier de la liste et affiche l'élément retiré,
+//  puis retire le premier de la liste. Affiche l'état final.
+
+let array1 = ['ghita', 'aicha', 'souad', 'fatima', 'leila', 'sondos'];
+
+console.log(array1);
